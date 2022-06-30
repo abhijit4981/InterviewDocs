fd145d338f8932b93f056829404fa5319eb21df2
abhijit4981 <abhijitmishra4981@gmail.com>
2022-06-30 15:47:47 +0530

    started reading flink basics

--- Apache/Camel/camel-demo/src/main/java/com/cosmos/cameldemo/route/ConsumeRestCallRoute.java
+++ Apache/Camel/camel-demo/src/main/java/com/cosmos/cameldemo/route/ConsumeRestCallRoute.java
@@ -1,7 +1,9 @@
 package com.cosmos.cameldemo.route;
 
 import com.cosmos.cameldemo.model.company.CompanyModel;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +11,19 @@ import org.springframework.stereotype.Component;
 public class ConsumeRestCallRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+        from("timer://test1?period=2000")
+                .process(exchange -> exchange.getIn().setBody(new Order(UUID.randomUUID().toString(), ThreadLocalRandom.current().nextInt(1, 10))))
+                .marshal().json(JsonLibrary.Gson)
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
+                .setHeader(Exchange., constant("application/json"))
+                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+                .to("https://www.nseindia.com/api/quote-equity?symbol=HCLTECH")
+                .process(exchange -> log.info("HTTP POST response is: {}", exchange.getIn().getBody(String.class)));
 
+        headers.set("accept-language", "n-US,en;q=0.9");
+        headers.set("accept-encoding", "zip,");
+        headers.set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+        headers.set("cookie", nseCookie);
         rest()
                 .get("https://www.nseindia.com/api/quote-equity?symbol=HCLTECH")
                 .type(CompanyModel.class)
