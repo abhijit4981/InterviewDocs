171f08c5216785e4c80422eaeb4363981fc0d91a
abhijit4981 <abhijitmishra4981@gmail.com>
2022-07-05 10:32:44 +0530

    Explored some more hook annotations

--- Java/Java11/java11/src/test/java/com/cosmos/LambdaExpressionPOCTest.java
+++ Java/Java11/java11/src/test/java/com/cosmos/LambdaExpressionPOCTest.java
@@ -1,25 +1,39 @@
 package com.cosmos;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LambdaExpressionPOCTest {
+    private LambdaExpressionPOC lambdaExpressionPOC;
+
+    @BeforeAll
+    public static void initBeforeAll(){
+        System.out.println("Runs before all i.e even the Class initialization so is a static");
+    }
+    @BeforeEach
+    public void init(){
+        lambdaExpressionPOC = new LambdaExpressionPOC();
+    }
+    @AfterEach
+    public void destroy(){
+        System.out.println("Cleaning up the objects..");
+    }
     @Test
     public void helloTest(){
         System.out.println("Hello From Junit5");
     }
     @Test
     public void minusTest(){
-        //Subtract subtract =
-        LambdaExpressionPOC lambdaExpressionPOC = new LambdaExpressionPOC();
         int expected = 15;
         int actual = lambdaExpressionPOC.subtraction(17,2);
         assertEquals(expected,actual);
     }
     @Test
     public void devideTest(){
-        LambdaExpressionPOC lambdaExpressionPOC = new LambdaExpressionPOC();
         assertThrows(ArithmeticException.class,()->lambdaExpressionPOC.devide(7,0),"U must not devide something with zero");
         //assertThrows(Exception.class,()->lambdaExpressionPOC.devide(7,0));
         //assertThrows(NullPointerException.class,()->lambdaExpressionPOC.devide(7,0));
